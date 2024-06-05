81d98b7694fa67d224fe82325b7b695d706911a2
Diksha Vodnala <dikshavodnala11@gmail.com>
2024-06-05 20:37:15 +0530

    project added

--- application.py
+++ application.py
@@ -53,4 +53,4 @@ def prediction():
             return render_template('home.html', results="An error occurred during prediction. Please try again.")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
